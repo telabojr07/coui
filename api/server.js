d7f19c676ac223521e2a36d2bb1268f4c0911623
ndowerdev <60291037+ndowerdev@users.noreply.github.com>
2022-03-30 00:44:32 +0700

    force https sitemap

--- api/server.js
+++ api/server.js
@@ -291,7 +291,7 @@ module.exports = async (req, res) => {
           } else {
             res.write(
               `<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="` +
-                originUrl +
+                originUrl.replace('http://','https://') +
                 `/assets/main-sitemap.xsl"?>
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`
             );
